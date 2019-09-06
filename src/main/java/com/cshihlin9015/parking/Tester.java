434a0fc0fe87c36e29b3cac73e00e552ce6c63d3
cshihlin9015 <cshihlin9015@gmail.com>
2019-09-06 10:55:29 +0800

    Java 8 新的日期時間處理(LocalDateTime)

--- src/main/java/com/cshihlin9015/parking/Tester.java
+++ src/main/java/com/cshihlin9015/parking/Tester.java
@@ -2,13 +2,36 @@ package com.cshihlin9015.parking;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Tester {
     public static void main(String[] args) {
 //        java7();
+//        java8();
+    }
 
+    private static void java8() {
+        Instant instant = Instant.now();
+        System.out.println(instant); //印出格林威治全球標準時間
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now); //印出本地時間
+
+        // 自行定義時間格式
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.println(formatter.format(now));
+
+        System.out.println(now.plus(Duration.ofHours(3))); // immutable，產生新的時間
+        System.out.println(now);
+
+        // 產生特定的日期與時間
+        LocalDateTime other = LocalDateTime.of(2019, 12, 25, 8, 0, 0);
+        System.out.println(other);
     }
 
     private static void java7() {
@@ -33,10 +56,10 @@ public class Tester {
         Calendar calendar = Calendar.getInstance();
         System.out.println(calendar);
         System.out.println(calendar.getTime()); // 呼叫 Calendar 身上的 getTime() 就可以得到 Date 物件
-        calendar.set(Calendar.MONTH,2);
-        calendar.set(Calendar.DAY_OF_MONTH,25);
+        calendar.set(Calendar.MONTH, 2);
+        calendar.set(Calendar.DAY_OF_MONTH, 25);
         System.out.println(calendar.getTime());
-        calendar.add(Calendar.DAY_OF_YEAR,8);
+        calendar.add(Calendar.DAY_OF_YEAR, 8);
         System.out.println(calendar.getTime());
     }
 }
